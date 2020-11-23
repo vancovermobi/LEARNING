894999397f94154dc90066d3b6155396157b2d55
LiemPD <dynamicliem@gmail.com>
2020-11-23 22:19:38 +0700

    lession7OneWayBinding

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-tutorial',
-    template: `
-            <h3>This is Tutorial_Component</h3>
-            <h4>Test style Component_Tutorial and Style index.html</h4>
+    template: `<h5>This is {{ title }}_Component</h5>
+        <img [src]="image" /><br>
+        <input type="text" #output (keyup)="0" [placeholder]="Template" />
+        <p>Output : {{ output.value }}</p>
     `,
     styles: [`
-            h3 {color:red}
+            h5 {color:red}
             h4 {color:blue}
     `]
 })
 
 export class TutorialComponent implements OnInit {
+    public title = "Tutorial";
+    public image = "https://bietmaytinh.com/wp-content/uploads/2019/12/game-hot-ran-san-moi-3D.png";
+    public Template = "Enter your name";
+
     constructor() { }
 
     ngOnInit() { }
