c8cf5dea572ef5cf82b21b5724b5f10550d35440
LiemPD <dynamicliem@gmail.com>
2020-11-24 18:58:29 +0700

    Lession 8

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-tutorial',
     template: `<h5>This is {{ title }}_Component</h5>
+        <!-- Lession 6 , Lession 7 -->
         <img [src]="image" /><br>
         <input type="text" #output (keyup)="0" [placeholder]="Template" />
         <p>Output : {{ output.value }}</p>
+
+        <!-- Lession 8 -->
+        <h4 [class.colorLession]="applyClass" >Lession 8 : Binding Class to element</h4>
+        <button (click)="RedColor();" applyClaas=true >RedColor</button><span>&nbsp;&nbsp;</span>
+        <button (click)="BlueColor();" applyClaas=false >BlueColor</button>
     `,
-    styles: [`
-            h5 {color:red}
-            h4 {color:blue}
+    styles: [`h5 {color:red} h4 {color:blue} .colorLession{ color: red}
     `]
 })
 
@@ -18,7 +22,13 @@ export class TutorialComponent implements OnInit {
     public image = "https://bietmaytinh.com/wp-content/uploads/2019/12/game-hot-ran-san-moi-3D.png";
     public Template = "Enter your name";
 
+    public applyClass : boolean = true;
     constructor() { }
-
     ngOnInit() { }
+    RedColor(){
+        this.applyClass= true;
+    }
+    BlueColor(){
+        this.applyClass= false;
+    }
 }
