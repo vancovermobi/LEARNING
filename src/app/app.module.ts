4304d8f094fc31c56a9044aaf1238bfa82c56e39
LiemPD <dynamicliem@gmail.com>
2020-11-24 21:11:25 +0700

    Lession 9 - 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TutorialComponent } from './tutorial.component';
@@ -10,7 +11,7 @@ import { TutorialComponent } from './tutorial.component';
     AppComponent, TutorialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -13,6 +13,11 @@ import { Component, OnInit } from '@angular/core';
         <h5 [style.color]="applyClass? 'blue' : 'orange'" >Style Color = If applyClass? = {{ applyClass }} BlueColor : OrangeColor</h5>
         <button (click)="RedColor();" applyClaas=true >RedColor</button><span>&nbsp;&nbsp;</span>
         <button (click)="BlueColor();" applyClaas=false >BlueColor</button>
+
+        <!-- Lession 9, 10 : ngModel  (import FormsModul to app.modul -->
+        <input type="text" [(ngModel)]="fname" /><span>&nbsp;&nbsp;</span>
+        <input type="text" [(ngModel)]="lname" />
+        <p>Full Name : {{ fname }} {{ lname }}</p>    
     `,
     styles: [`h5 {color:red} h4 {color:blue} .colorLession{ color: red}
     `]
@@ -22,8 +27,8 @@ export class TutorialComponent implements OnInit {
     public title = "Tutorial";
     public image = "https://bietmaytinh.com/wp-content/uploads/2019/12/game-hot-ran-san-moi-3D.png";
     public Template = "Enter your name";
-
     public applyClass : boolean = true;
+    public fname: String ; lname : String ;
     constructor() { }
     ngOnInit() { }
     RedColor(){
