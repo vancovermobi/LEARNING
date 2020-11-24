3c76c6b862c7a72b550e76d731e76c8a6ab79f3a
LiemPD <dynamicliem@gmail.com>
2020-11-24 23:19:52 +0700

    Lession 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TutorialComponent } from './tutorial.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LEARNING';
+  public names = ['Mr A', 'Mr B', 'Mr C', 'Mr D', 'Mr E'];
+  public agree:number = 0 ;
+  public disgree:number = 0 ;
+
+  ParentVote(vote:boolean){
+    if(vote) this.agree++;
+    else this.disgree++;
+  }
+  @ViewChild(TutorialComponent)
+    private tutorialComponent: TutorialComponent;
+  ChangeName(txtname){
+    this.tutorialComponent.setName(txtname);
+  }
 }
+
--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -1,39 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
     selector: 'app-tutorial',
     template: `<h5>This is {{ title }}_Component</h5>
-        <!-- Lession 6 , Lession 7 -->
-        <img [src]="image" /><br>
-        <input type="text" #output (keyup)="0" [placeholder]="Template" />
-        <p>Output : {{ output.value }}</p>
+        <!-- Lession 13 , Lession 14 -->
+        <p>Tutorial_Input : {{ name }}</p>
 
-        <!-- Lession 8 -->
-        <h4 [class.colorLession]="applyClass"  >Lession 8 : Binding Class to element</h4>
-        <h5 [style.color]="applyClass? 'blue' : 'orange'" >Style Color = If applyClass? = {{ applyClass }} BlueColor : OrangeColor</h5>
-        <button (click)="RedColor();" >RedColor</button><span>&nbsp;&nbsp;</span>
-        <button (click)="BlueColor();">BlueColor</button>
-
-        <!-- Lession 9, 10 : ngModel  (import FormsModul to app.modul -->
-        <input type="text" [(ngModel)]="fname" /><span>&nbsp;&nbsp;</span>
-        <input type="text" [(ngModel)]="lname" />
-        <p>Full Name : {{ fname }} {{ lname }}</p>   
-
-         <!-- Lession 11, 12 : Structural Directives , Attribute Directives  -->
-         <h4 *ngIf="showLineIf" >This Structural Directives: *ngIf </h4>
-         <p>ShowLine : {{ showLineIf }}</p>
-         <button (click)="showLineIf=true ; color='red'"  >Show</button><span>&nbsp;&nbsp;</span>
-         <button (click)="showLineIf=false; color='blue'" >Hidden</button>
-         <br>
-         <div [ngSwitch]="color" >
-            <p *ngSwitchCase="'red'" >This line Color is red</p>
-            <p *ngSwitchCase="'blue'" >This line Color is blue</p>
-            <p *ngSwitchCase="'green'" >This line Color is green</p>
-            <p *ngSwitchDefault>Invalid Color </p>
-         </div>
-
-         <p [ngClass]="{ classOne:C1 , classTwo:C2 }">Attribute . This ngClass apply style </p>
-         <button (click)="C1=!C1; C2=!C2" >ChangeClass</button>
+        <button [disabled]="voted" (click)="vote(true)" >Agree</button>
+        <button [disabled]="voted" (click)="vote(false)" >Disgree</button>
+        <p>Result : {{ result }}</p>
     `,
     styles: [`h5 {color:red} h4 {color:blue} .colorLession{ color: red}
             .classOne{color: yellow; } .classTwo{background-color:black;}
@@ -42,19 +17,18 @@ import { Component, OnInit } from '@angular/core';
 
 export class TutorialComponent implements OnInit {
     public title = "Tutorial";
-    public image = "https://bietmaytinh.com/wp-content/uploads/2019/12/game-hot-ran-san-moi-3D.png";
-    public Template = "Enter your name";
-    public applyClass : boolean = true;
-    public fname: String ; lname : String ;
-    public showLineIf : boolean ;
-    public color = "";
-    public C1 = true; C2 = true;
+    @Input() name: String;
+    public voted : boolean = false;
+    public result : string;
+    @Output() onVote = new EventEmitter<boolean>();
     constructor() { }
     ngOnInit() { }
-    RedColor(){
-        this.applyClass= true;
+    vote(vote:boolean){
+        this.voted = true;
+        this.result = vote ? 'agree' : 'disgree' ;
+        this.onVote.emit(vote);
     }
-    BlueColor(){
-        this.applyClass= false;
+    setName(name:string){
+        this.name = name ;
     }
 }
