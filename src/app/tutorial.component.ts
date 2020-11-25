e4b94d6e820e9368ef69d282224096b98553cb57
LiemPD <dynamicliem@gmail.com>
2020-11-25 21:21:06 +0700

    Lession 16

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -1,49 +1,89 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-    selector: 'app-tutorial',
-    template: `<h5>This is {{ title }}_Component</h5>
-        <!-- Lession 15 , Lession 16 -->
-       <form #userForm="ngForm" (ngSubmit)="onSubmit(userForm.value)" >
-          <div class="form-group" >
-            <label for="name">Name</label>
-            <input #name name="name" ngModel type="text" class="form-control" />
-          </div>  
-          <div class="form-group" >
-            <label for="email">Email</label>
-            <input #email name="email" ngModel type="text" class="form-control" />
-          </div>  
-          <div class="form-group" >
-            <label for="street">Street</label>
-            <input #street name="street" ngModel type="text" class="form-control" />
-          </div>  
-          <div class="form-group" >
-            <label for="city">City</label>
-            <input #city name="city" ngModel type="text" class="form-control" />
-          </div> 
-          <div class="form-group" >
-            <label for="postalcode">Postal Code</label>
-            <input #postalcode name="postalcode" ngModel type="text" class="form-control" />
-          </div>  
-          <button type="submit" class="btn btn-primary" >Submit</button> 
-       </form>
-        <p>Result : {{ result }}</p>
+  selector: 'app-tutorial',
+  template: `<h5>This is {{ title }}_Component</h5>
+    <!-- Lession 16 , 17-->
+    <form #userForm="ngForm" (ngSubmit)="onSubmit(userForm.value)">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input required #name="ngModel" name="name" ngModel type="text" class="form-control" />
+        <!-- {{ name.className }} -->
+        <div *ngIf="name.errors && (name.dirty || name.touched)" class="label label-danger" >
+          <div [hidden]="!name.errors.required" class="label label-danger">
+            Name is required
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input required #email name="email" ngModel type="text" class="form-control" />
+      </div>
+      <div class="form-group">
+        <label for="street">Street</label>
+        <input #street name="street" ngModel type="text" class="form-control" />
+      </div>
+      <div class="form-group">
+        <label for="city">City</label>
+        <select required #city name="city" ngModel type="text" class="form-control">
+            <option *ngFor="let city of Citis" [value]="city.Id">{{ city.Name }}</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="postalcode">Postal Code</label>
+        <input pattern="^[1-9][0-9]{4}$" #postalcode="ngModel" name="postalcode" ngModel type="text" class="form-control" />
+        <div *ngIf="postalcode.errors && (postalcode.dirty || postalcode.touched)" class="label label-danger" >
+          <div [hidden]="!postalcode.errors.pattern" class="label label-danger">
+            Postalcode is required must be have 5 digits
+          </div>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <p>Result : {{ result }}</p> `,
+  styles: [
+    `
+      h5 {
+        color: red;
+      }
+      h4 {
+        color: blue;
+      }
+      .colorLession {
+        color: red;
+      }
+      .classOne {
+        color: yellow;
+      }
+      .classTwo {
+        background-color: black;
+      }
+      .ng-valid[required],
+      .ng-valid.required {
+        border-left: 5px solid #42a948; /* green */
+      }
+
+      .ng-invalid:not(form) {
+        border-left: 5px solid #a94442; /* red */
+      }
     `,
-    styles: [`h5 {color:red} h4 {color:blue} .colorLession{ color: red}
-            .classOne{color: yellow; } .classTwo{background-color:black;}
-    `]
+  ],
 })
-
 export class TutorialComponent implements OnInit {
-    public title = "Tutorial";
-    public result : any;
-    @Output() onVote = new EventEmitter<boolean>();
-    constructor() { }
-    ngOnInit() { }
-    
-    onSubmit(formValue: any){
-        this.result = formValue.name ;
-        console.log( this.result);
+  public title = 'Tutorial';
+  public result: any;
+  public Citis = [{Id: 1, Name:'Hà Nội'},
+                  {Id: 1, Name:'Hồ Chí Minh'}, 
+                  {Id: 1, Name:'Hải Phòng'},
+                  {Id: 1, Name: 'Đà Nẵng'},
+                  {Id: 1, Name: 'Cần Thơ'}
+                ];
+  @Output() onVote = new EventEmitter<boolean>();
+  constructor() {}
+  ngOnInit() {}
 
-    }
+  onSubmit(formValue: any) {
+    this.result = formValue.name;
+    console.log(this.result);
+  }
 }
